fced4e1d27177934758af5b13e631f34c312d044
LUIS CESAR HRYCKIV <luiscesarhryckiv@gmail.com>
2022-08-09 19:33:42 -0300

    Praticando variáveis

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let nome = "Jhon";
 let sobrenome = "Doyle";
-let anoNascimento = "1991";
-let idade = 2022 - 1991;
+let anoNascimento = 1991;
+let idade = 2022 - anoNascimento;
 let cidade = "Curitiba";
 
 const JhonFrase = `Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`;
+
+console.log(JhonFrase);
